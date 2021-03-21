ae1850a59f63cc317a089a805fef776418e24e9a
pawcio <pawlaty@gmail.com>
2021-03-21 20:31:32 +0100

    Plik modelu polaczenia z allegro api

--- src/Model/Allegro/Allegro.php
+++ src/Model/Allegro/Allegro.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ *
+ * By Pawlaty na podstawie dokumentacji cakephp 4.* oraz dokmumentacji ApiAllegro
+ *
+ *
+ */
 
 namespace App\Model\Allegro;
 
@@ -8,117 +14,131 @@ use Cake\Form\Form;
 use Cake\Form\Schema;
 use Cake\Validation\Validator;
 
+
+/*
+*Klasa odpowiedzailna za połączenie z Api alegro i podstawowe zapytania
+*/
+
+
 class Allegro extends Form
 {
+    private $result = array();
     /*
+    *funkcja z dokumentacji -- niewykorzystana
     protected function _buildSchema(Schema $schema):_buildSchema
     {
         return $schema->addFleld('name','string')=>addField('email'['type'=>'string'])=>addField('body',['type'=>'text']);
     }
     */
     /*
+    **funkcja z dokumentacji -- niewykorzystana
     public function validationDefault(Validator $validator):Validator
     {
         $validator->minLength('name',10)->email('email');
         return $validator;
     }
     */
-
+    /*
+    *funkcja z dokumentacji -- niewykorzystana
+    */
     protected function _execute(array $data):bool
     {
         //send the email
         return true;
     }
 
+    /*
+    *sprawdza czy jest odpowiedz i czy statut jest 200
+    *jezeli tak zwraca true i ustawia zmienne w tablicy asocjacyjnej
+    *result na wartosci pobrane w zapytaniu
+    */
+    private function CheckResult($response):bool
+    {
 
-    public function GetToken()
+        $this->result['responseJSON'] = $response->getJson();
+        $this->result['code'] = $response->getStatusCode();
+        $this->result['headers'] = $response->getHeaders();
+
+        if($response === false ||  $this->result['code'] !== 200)
         {
-        $http = new Client(['headers' => ['Content-Type' => 'application/json']]);
-        $response = $http->get('https://allegro.pl.allegrosandbox.pl/auth/oauth/token?grant_type=client_credentials',[],
-                                ['auth'=> ['username' => '3fe9b1c100c143b393c1281fd91361d3',
-                                            'password'=>'HxkKsIfSoP74VKJWaoXI6NBMQU9fIPwPiOI1otFhwK7ywokXxYuHwIeIzxhUCFEC'
-                                          ]]);
-        $result = $response->getJson();
-       // return $result['access_token'];
-        return $result;
-    }
-
-    public function FindItem($words){
-        /*
-        if(!is_array($words)){
-            exit('wrong type parametrs');
-        }*/
-
-       // $words_str = implode(' ',$words);
-        $http = new Client(['headers' => ['Content-Type' => 'application/json']]);
-        $response = $http->get("https://api.allegro.pl/offers/listing?phrase={$words}&searchMode=REGULAR&fallback=true&include=-all&include=filters",[],
-                               ['auth'=> ['username' => '3fe9b1c100c143b393c1281fd91361d3',
-                                          'password'=>'HxkKsIfSoP74VKJWaoXI6NBMQU9fIPwPiOI1otFhwK7ywokXxYuHwIeIzxhUCFEC'
-                                         ]]);
-        $result = $response->getJson();
-
-        return $result;
-
-    }
-
-    public function GetCategories(String $token):array
-    {   // $getCategoriesUrl = "https://api.allegro.pl/allegrosandbox.pl/order/events";
-        //$getCategoriesUrl   = "https://api.allegro.pl.allegrosandbox.pl/offers/listing";
-        $getCategoriesUrl = "https://api.allegro.pl.allegrosandbox.pl/sale/categories";
-
-        //$bearer = 'Bearer '.$token;
-
-        $http = new Client();         //'Content-Type' => 'application/json']]);
-        //$http->addHeaders(['Authorization'=>$bearer, "Accept"=>"application/vnd.allegro.public.v1+json"]);
-
-        $response = $http->get($getCategoriesUrl,[],['headers' => ['Title'=>'my title','Authorization'=>'Bearer '.$token, "Accept"=>"application/vnd.allegro.public.v1+json"]]);
-/*
-Ten problem pojawia się tylko w tym zasobie czy też w innych zasobach? Prosimy o podanie nam identyfikatora śledzenia z połączenia, jeśli istnieje problem. Wyślij również do nas (możesz to zrobić za pomocą naszego formularza kontaktowego ) cURL z zapytaniem i odpowiedzią.
-
-Jeśli wyślę żądanie do testowego API ...
-
-Masz na myśli Allegro Sandbox? W pierwszej kolejności należy zmienić adres zasobu na https://api.allegro.pl.allegrosandbox.pl/ . To jest adres zasobów w Sandbox.
-
-Powinieneś także użyć poświadczeń również z Sandbox . Jakich danych logowania użyłeś w tej prośbie?
-
-*/
-        $result = array();
-        $result['responseJSON'] = $response->getJson();
-        $result['code'] = $response->getStatusCode();
-        $result['headers'] = $response->getHeaders();
-/*
-        $ch = curl_init($getCategoriesUrl);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, [
-                            "Authorization: Bearer $token",
-                            "Accept: application/vnd.allegro.public.v1+json"
-        ]);
-
-        $maincategoriesresult = curl_exec($ch);
-        $result_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-       // $result_message = curl_getinfo($ch, CURLINFO_HTTP_MESSAGE);
-
-        curl_close($ch);
-*/
-        if($response === false ||  $result['code'] !== 200)
-        {
-            echo "code: ". $result['code']."<br>";
+            echo "code: ". $this->result['code']."<br>";
             echo "message: <br>";
 
             echo "<pre>";
-            print_r($result['headers']);
+            print_r($this->result['headers']);
             echo "</pre>";
             echo 'result its:';
             echo "<pre>";
-            print_r($result['responseJSON']);
+            print_r($this->result['responseJSON']);
             echo "</pre>";
-
+            return false;
             exit();
         }
-/*      $categories_list = json_decode($maincategoriesresult);          */
+        return true;
+    }
 
-        //return (object)$result['responseJSON'];
-        return $result['responseJSON'];
+    /*
+    *zwraca tokena po autoryzacji
+    *
+    */
+    public function GetToken()
+    {
+        $http = new Client(['headers' => ['Content-Type' => 'application/json']]);
+        $response = $http->get('https://allegro.pl.allegrosandbox.pl/auth/oauth/token?grant_type=client_credentials',[],
+                                ['auth'=> ['username' => '**************61d3',
+                                            'password'=>'**********************uHwIeIzxhUCFEC'
+                                          ]]);
+        $this->result = $response->getJson();   //dostęp do pola obiektu JSON: $result['access_token'];
+
+        return $this->result;
+    }
+
+
+    /*
+    *Pobiera kategorie z sandboxa allegro i zwraca jako tablica
+    */
+    public function GetCategories(String $token):array
+    {
+        // $getCategoriesUrl = "https://api.allegro.pl/allegrosandbox.pl/order/events";
+        //$getCategoriesUrl   = "https://api.allegro.pl.allegrosandbox.pl/offers/listing";
+        $getCategoriesUrl = "https://api.allegro.pl.allegrosandbox.pl/sale/categories";
+
+        $http = new Client();
+
+        $response = $http->get($getCategoriesUrl,[],['headers' => ['Title'=>'my title','Authorization'=>'Bearer '.$token, "Accept"=>"application/vnd.allegro.public.v1+json"]]);
+
+        $this->CheckResult($response);
+
+        // rzutowanie  zwróconego obiektu na stdClass: (object)$result['responseJSON'];
+        //zwrócenie tablicy wyników:
+        return $this->result['responseJSON'];
 
     }
+
+    /*
+    *Znalezienie pozycji po slowach kluczowych
+    */
+    public function FindItem(String $token, String $words):array
+    {
+
+        $getItemByWord = "https://api.allegro.pl.allegrosandbox.pl/offers/listing?phrase={$words}&searchMode=REGULAR&fallback=true&include=-all&include=filters";
+        /*
+        if(!is_array($words)){
+            exit('wrong type parametrs');
+        }
+        !trzeba bedzie sprawdzic zapytnie dla wszystkich slow
+        $words_str = implode(' ',$words);*/
+
+        $http = new Client();
+        $response = $http->get("https://api.allegro.pl/offers/listing?phrase={$words}&searchMode=REGULAR&fallback=true&include=-all&include=filters",[],
+                               ['headers' => ['Title'=>'my title','Authorization'=>'Bearer '.$token, "Accept"=>"application/vnd.allegro.public.v1+json"]]);
+
+        $this->CheckResult($response);
+        $result = $response->getJson();
+
+        return $this->result['responseJSON'];
+
+    }
+
+
 }
